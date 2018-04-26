485bb221f37ee37be00e818a4dd5c2a5a5feaf59
Jing Chun Tan <jtan1993@gmail.com>
2018-04-26 17:27:43 +0100

    Update triangles.cs

--- triangles.cs
+++ triangles.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-//to display the mesh: the game object needs a renderer and a mesh filter to pass the mesh info to the renderer  
+//to display the mesh: the game object needs a renderer and a mesh filter to pass the mesh info to the renderer 
+//create necessary components automatically
 [RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshRenderer))]  
 
 //call the update method whenever there're some changes in the scene
@@ -15,15 +16,15 @@ public class Triangles : MonoBehaviour{
   [SerializeField] private Material material;   
   private List<Vector3> vertices;
   private List<int> triangles;
-  private MeshFilter meshFilter;
+  private MeshFilter meshFilter; 
   private MeshRenderer meshRenderer;
   private Mesh mesh; //store the mesh info - vertices, triangles, normals
   
   void Update(){
-  //get references
+  //mesh generating code - gets updated whenever a value is changed in the inspector, gets references 
     meshFilter = GetComponent<MeshFilter>(); 
     meshRenderer = GetComponent<MeshRenderer>(); 
-    meshRenderer.material = material;
+    meshRenderer.material = material; //assign created material to material property and display the model
     
     //init
     vertices = new List<Vector3>();
